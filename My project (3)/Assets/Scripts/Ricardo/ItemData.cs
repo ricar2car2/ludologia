110c32ea14a460a23a0de0d6a240c21629a773fa
ricar2car2 <ricarsecond2@gmail.com>
2025-05-23 17:12:07 +0200

    new inventory

--- My project (3)/Assets/Scripts/Ricardo/ItemData.cs
+++ My project (3)/Assets/Scripts/Ricardo/ItemData.cs
@@ -5,44 +5,44 @@ using System.Collections.Generic;
 public class ItemData : MonoBehaviour
 {
     [Header("Setup")]
-    public int itemID, requiredItemID; // ID del ítem y ID requerido para recogerlo
+    public int itemID, requiredItemID; // ID del item y ID requerido para recogerlo
 
     [Header("Exito")]
 
-    // Objetos que se deben desactivar o activar si el ítem es recogido exitosamente
+    // Objetos que se deben desactivar o activar si el item es recogido exitosamente
     public GameObject[] objectsToRemove;
     public GameObject[] objectsToSetActive;
 
-    // Sprite que representa este ítem en el equipo
+    // Sprite que representa este item en el equipo
     public Sprite itemSlotSprite;
 
     [Header("Tipo de objeto")]
     public bool isNPC = false;
 
 
-    // Nombre que aparece cuando el ítem aún no ha sido recogido
+    // Nombre que aparece cuando el item aun no ha sido recogido
     public string objectName;
 
-    // Tamaño del nombre que aparece antes de ser recogido
+    // Tamano del nombre que aparece antes de ser recogido
     public Vector2 nameTagSize = new Vector2(3, 0.65f);
 
-    // Nombre del ítem cuando ya fue recogido
+    // Nombre del item cuando ya fue recogido
     public string itemName;
 
-    // Tamaño del nombre una vez recogido
+    // Tamano del nombre una vez recogido
     public Vector2 ItemNameTagSize = new Vector2(3, 0.65f);
 
     [Header("Failure")]
 
-    // Mensaje que se muestra si no se puede recoger el ítem
+    // Mensaje que se muestra si no se puede recoger el item
     [TextArea(3, 3)]
     public string hintMessage;
 
-    [Header("Diálogo")]
+    [Header("Dialogo")]
     [TextArea(2, 4)]
     public List<string> dialogue;
 
-    // Tamaño del cuadro de texto del hint
+    // Tamano del cuadro de texto del hint
     public Vector2 hintBoxSize = new Vector2(4, 0.65f);
 
 
